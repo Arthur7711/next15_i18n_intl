14b9130e567921925eb5c532fa7fddcb83c14084
Arthur <newartroll11@gmail.com>
2025-09-07 20:58:11 +0400

    feat: config for next_intl

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+
+export default async function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Home page</h1>
+    </div>
+  );
+}
---
+++ i18n/request.ts
@@ -0,0 +1,17 @@
+import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ locale }) => {
+  const requested = await locale;
+  const localeName = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
+
+  const messages = await import(`../messages/${localeName}.json`);
+
+  return {
+    locale: localeName,
+    messages: messages.default,
+  };
+});
